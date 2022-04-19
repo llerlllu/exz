7b0b0ebbfbf9ff52c87882251eb52d4b65d34b2f
SaZyGH <103922719+SaZyGH@users.noreply.github.com>
2022-04-19 09:29:31 +0300

    ekz

---
+++ ClassLibrary/Application.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Application
+    {
+        private Application application;
+        private List<TaskList> taskList;
+
+        public Application()
+        {
+            TaskList taskList = new TaskList("Входящие");
+            taskLists = new List<TaskList>();
+            taskLists.Add(taskList);
+        }
+
+        public Application GetApplication()
+        {
+            if (application != null)
+            {
+                return application;
+            }
+            application = new Application();
+            return application;
+        }
+
+        public void CreateTaskList (string name)
+        {
+            TaskList taskList = new TaskList(name);
+            taskLists.Add(taskList);
+        }
+        public TaskList GetTaskListNames (string name)
+        {
+            return taskLists.FirstOrDefault(taskList => taskList.GetName() == name);
+        }
+    }
+}
---
+++ ClassLibrary/TaskList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class TaskList
+    {
+        private string name;
+        private List<Task> tasks;
+
+        public TaskList(string name)
+        {
+            this.name = name;
+            tasks = new List<Task>();
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+        public List<Task> GetTasks()
+        {
+            return tasks;
+        }
+        public void Remove(Task task)
+        {
+            tasks.Remove(task);
+        }
+        public List<Task> GetTasksByToday()
+        {
+            List<Task> tasksByToday = new List<Task>();
+            foreach (var task in tasks)
+            {
+                if (task.Due == DateTime.Now)
+                {
+                    tasksByToday.Add(task);
+                }
+            }
+            return tasksByToday;
+        }
+        public List<Task> GetTasksByFuture()
+        {
+            List<Task> tasksByFuture = new List<Task>();
+            foreach (var task in tasks)
+            {
+                if (task.Due == DateTime.Now)
+                {
+                    tasksByFuture.Add(task);
+                }
+            }
+            return tasksByFuture;
+        }
+    }
+}
